f677168c8af8c968f8f4f184ea3a0fa4bd56b27d
CarolineGr <30620910+CarolineGr@users.noreply.github.com>
2017-08-08 11:56:29 +0200

    Add files via upload

--- laccordeur.sql
+++ laccordeur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 07 Août 2017 à 10:37
+-- Généré le :  Mar 08 Août 2017 à 09:54
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -47,6 +47,36 @@ CREATE TABLE `livreor_commentaires` (
   `id_utilisateur` int(10) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `livreor_commentaires`
+--
+
+INSERT INTO `livreor_commentaires` (`id`, `Messages`, `Dates`, `id_utilisateur`) VALUES
+(1, 'blabla', '2017-08-04 09:53:16', 1),
+(2, 'bienvenue !', '2010-09-13 21:04:00', 1),
+(3, 'J\'aime !', '2010-09-05 21:04:12', 1),
+(4, 'Encore une fois.', '2010-09-02 21:04:45', 1),
+(5, 'Ceci est un commentaire.', '2010-09-04 21:05:01', 1),
+(6, 'Il etait une fois....', '2010-09-05 21:05:23', 1),
+(7, 'Bienvenue a vous tous !', '2010-09-01 21:05:51', 1),
+(8, 'Salut, super ce site !', '2010-09-02 21:06:09', 1),
+(9, 'Il etait une fois...', '2010-09-05 21:06:33', 1),
+(10, 'Jamais deux sans toi !', '2010-09-12 21:06:59', 1),
+(11, 'Salut, j\'aime bien ce site !', '2010-09-21 21:08:16', 1),
+(12, 'Debut de site sympathique.', '2010-09-13 21:08:13', 1),
+(13, 'Encore une fois.', '2010-09-02 21:04:45', 1),
+(14, 'Ceci est un commentaire.', '2010-09-04 21:05:01', 1),
+(15, 'Il etait une fois....', '2010-09-05 21:05:23', 1),
+(16, 'Bienvenue a vous tous !', '2010-09-01 21:05:51', 1),
+(17, 'Salut, super ce site !', '2010-09-02 21:06:09', 1),
+(18, 'Il etait une fois...', '2010-09-05 21:06:33', 1),
+(19, 'Jamais deux sans toi !', '2010-09-12 21:06:59', 1),
+(20, 'Salut, j\'aime bien ce site !', '2010-09-21 21:08:31', 1),
+(21, 'Debut de site sympathique.', '2010-09-13 21:08:13', 1),
+(22, 'Encore une fois.', '2010-09-02 21:04:45', 1),
+(23, 'Ceci est un commentaire.', '2010-09-04 21:05:01', 1),
+(24, 'Il etait une fois....', '2010-09-05 21:05:23', 1),
+(25, 'Bienvenue a vous tous !', '2010-09-01 21:05:51', 1);
 
 -- --------------------------------------------------------
 
@@ -60,7 +90,7 @@ CREATE TABLE `utilisateurs` (
   `nom` varchar(20) NOT NULL,
   `prenom` varchar(20) NOT NULL,
   `email` varchar(20) NOT NULL,
-  `mdp` varchar(30) NOT NULL,
+  `mdp` varchar(60) NOT NULL,
   `statut` tinyint(1) DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -69,7 +99,7 @@ CREATE TABLE `utilisateurs` (
 --
 
 INSERT INTO `utilisateurs` (`id`, `pseudo`, `nom`, `prenom`, `email`, `mdp`, `statut`) VALUES
-(1, 'Kro', 'GRI', 'Caro', 'caro@gmail.com', 'cg', 0);
+(1, 'Kro', 'GRIMAULT', 'Caroline', 'caro@gmail.com', '$1$oz3.RA4.$XGQ5LfWVL9WA2Nv1KLkTm0', 0);
 
 --
 -- Index pour les tables exportées
